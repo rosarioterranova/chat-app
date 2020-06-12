1f9972bed17b97b4996b106083a3c072169aeb8c
Rosario Terranova <rosarioterranova91@gmail.com>
2020-06-12 21:16:58 +0200

    added rooms

--- src/index.js
+++ src/index.js
@@ -1,31 +1,73 @@
-const http = require("http")
-const path = require("path")
-const express = require("express") 
-const socketio = require("socket.io")
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = express()
-const server = http.createServer(app) //create a server
-const io = socketio(server) //link the socket to that server
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.json())
-app.use(express.static(path.join(__dirname, "../public"))) //set the public folder
+app.use(express.static(publicDirectoryPath))
 
-let count = 0
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
 
-//Event fired when a new client  is connected
-io.on("connection", (socket) =>{ //socket is an obj with connection info
-    console.log("New WebSocket connection")
-    
-    socket.emit("countUpdated", count) //emmit an event to the clients and send "count" data
-    
-    socket.on("increment", () => { //recevied event from client
-        count++
-        io.emit("countUpdated", count) //emit the event to ALL clients connected (with socket just with a specific client)
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options })
+
+        if (error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
+
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+
+        callback()
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.to(user.room).emit('message', generateMessage(user.username, message))
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
     })
 })
 
-server.listen(port, () => { //link to the custom server with the socket
-    console.log("Server is running on port " + port)
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
 })
